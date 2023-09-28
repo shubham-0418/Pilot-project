4d650fa4563fe2120bf95e4ee91bb7be9581aacb
SourabhAngadi <141898486+SourabhAngadi@users.noreply.github.com>
2023-09-28 16:41:50 +0530

    commit

--- src/test/java/com/Runner/RunnerClass.java
+++ src/test/java/com/Runner/RunnerClass.java
@@ -1,20 +1,24 @@
 package com.Runner;
 
+import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 public class RunnerClass {
 	@CucumberOptions(
 
-
 			features = {"src/test/resources/FeatureFile/Login.feature","src/test/resources/FeatureFile/wishlist.feature","src/test/resources/FeatureFile/School_Education.feature","src\\test\\resources\\FeatureFile\\Company.feature","src/test/resources/FeatureFile/children.feature","src\\test\\resources\\FeatureFile\\Support.feature"},
-
 			glue="com.StepDefinition",
 			plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-			tags= "@ShopByClassNew"
+			tags= ""
 
 			)
 	public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
+		@Override
+	    @DataProvider(parallel = true)
+	    public Object[][] scenarios() {
+	        return super.scenarios();
+		}
 
 	}
 }
